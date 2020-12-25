ccbcbe00650b69a54c115ce20c363eb4392c4f00
andrey30353 <andrey30353@mail.ru>
2020-12-25 18:03:04 +0300

    Генерация префабов внутри круга

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -4,5 +4,22 @@ using UnityEngine;
 
 public class Main : MonoBehaviour
 {
-    public float Radius = 10;
+    [SerializeField] private Connectable _connectablePrefab;
+    [SerializeField] private float _radius = 10;
+    [SerializeField] private float _count = 10;
+
+    private void Start()
+    {
+        for (int i = 0; i < _count; i++)
+        {
+            var randomPosition = Random.insideUnitCircle * _radius;
+            var position = new Vector3(randomPosition.x, 0, randomPosition.y);
+            Instantiate(_connectablePrefab, position, Quaternion.identity);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, _radius);
+    }
 }
